15375d3a91e4b1bbdb03e3be972ea756afb6cb25
faxcon08 <faxcon-08@mail.ru>
2023-04-16 15:44:30 +0300

    add new Branch

--- src/main/java/ru/skypro/lesson_28/Application.java
+++ src/main/java/ru/skypro/lesson_28/Application.java
@@ -3,6 +3,6 @@ import java.sql.*;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("---------First Task---------------");
+        System.out.println("-----------First Task---------------");
     }
 }
